b6783ef5276589f06e9bbb658a75e62f5fe42f1c
Benjamin Barbeau <73616169+Ategon@users.noreply.github.com>
2021-12-23 14:33:20 -0500

    Add character select background

--- boredjam/Assets/Scripts/PlayerController.cs
+++ boredjam/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,20 @@ public class PlayerController : MonoBehaviour
     private GameObject characterSelect;
     private float changeCooldown = 0;
 
+    [Header("Variables")]
+    [SerializeField] private bool p1Escape;
+    [SerializeField] private bool p2Escape;
+    [SerializeField] private bool p1Jump;
+    [SerializeField] private bool p2Jump;
+    [SerializeField] private float p1Move;
+    [SerializeField] private float p2Move;
+    [SerializeField] private bool p1Attack;
+    [SerializeField] private bool p2Attack;
+    [SerializeField] private bool p1Fireball;
+    [SerializeField] private bool p2Fireball;
+    [SerializeField] private bool p1Dash;
+    [SerializeField] private bool p2Dash;
+
     void Start()
     {
         switch (playerInput.playerIndex)
@@ -30,6 +44,9 @@ public class PlayerController : MonoBehaviour
         }
 
         //TODO update visual of character select
+        characterSelect.transform.Find("Press Text").gameObject.SetActive(false);
+        characterSelect.transform.Find("Character").gameObject.SetActive(true);
+        characterSelect.transform.Find("Colors").gameObject.SetActive(true);
     }
     
 

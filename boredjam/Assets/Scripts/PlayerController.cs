11fa1a82e68cdb5aec9a0dc9613c545fd48b7aa8
Benjamin Barbeau <73616169+Ategon@users.noreply.github.com>
2021-12-23 23:59:20 -0500

    Add palette selection

--- boredjam/Assets/Scripts/PlayerController.cs
+++ boredjam/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,27 +12,107 @@ public class PlayerController : MonoBehaviour
 
     private GameObject characterSelect;
     private float changeCooldown = 0;
+    private bool escapeBuffer = false;
+    private bool jumpBuffer = true;
+    private bool attackBuffer = true;
 
-    [Header("Variables")]
-    [SerializeField] private bool p1Escape;
-    [SerializeField] private bool p2Escape;
-    [SerializeField] private bool p1Jump;
-    [SerializeField] private bool p2Jump;
-    [SerializeField] private float p1Move;
-    [SerializeField] private float p2Move;
-    [SerializeField] private bool p1Attack;
-    [SerializeField] private bool p2Attack;
-    [SerializeField] private bool p1Fireball;
-    [SerializeField] private bool p2Fireball;
-    [SerializeField] private bool p1Dash;
-    [SerializeField] private bool p2Dash;
+    private bool splitKeyboard = false;
+    private bool escape;
+    
+    private Vector2 p1Move;
+    private bool p1Jump;
+    private bool p1Attack;
+    private bool p1Fireball;
+    private bool p1Dash;
+    private Vector2 p2Move;
+    private bool p2Jump;
+    private bool p2Attack;
+    private bool p2Fireball;
+    private bool p2Dash;
 
-    [SerializeField] private int p1ColorSelectIndex;
-    [SerializeField] private int p2ColorSelectIndex;
-    [SerializeField] private List<PaletteColor> paletteColors;
+    private int p1ColorSelectIndex;
+    private int p2ColorSelectIndex;
+    private List<PaletteColor> paletteColors;
+
+    private int selectingState = 0;
+
+    private PlayerManager playerManager;
+
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1f)
+        {
+            p1Jump = true;
+        }
+        else
+        {
+            p1Jump = false;
+        }
+    }
+
+    public void OnDash(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1f)
+        {
+            p1Dash = true;
+        }
+        else
+        {
+            p1Dash = false;
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1f)
+        {
+            p1Attack = true;
+        }
+        else
+        {
+            p1Attack = false;
+        }
+    }
+
+    public void OnFireball(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1f)
+        {
+            p1Fireball = true;
+        }
+        else
+        {
+            p1Fireball = false;
+        }
+    }
+
+    public void OnEscape(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1f)
+        {
+            escape = true;
+        }
+        else
+        {
+            escape = false;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        p1Move = ctx.ReadValue<Vector2>();
+    }
+
+
 
     void Start()
     {
+
+        playerManager = GameObject.Find("Player Manager").GetComponent<PlayerManager>();
+        playerManager.activePlayers++;
+        DontDestroyOnLoad(this.gameObject);
+
         switch (playerInput.playerIndex)
         {
             case 0:
@@ -52,7 +133,7 @@ public class PlayerController : MonoBehaviour
         characterSelect.transform.Find("Character").gameObject.SetActive(true);
         characterSelect.transform.Find("Colors").gameObject.SetActive(true);
 
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 7; i++)
         {
             PaletteColor newColor = new PaletteColor();
 
@@ -115,22 +196,16 @@ public class PlayerController : MonoBehaviour
             paletteColors.Add(newColor);
         }
 
-        characterSelect.transform.Find("Character").Find("Hair L").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].hairL;
-        characterSelect.transform.Find("Character").Find("Hair M").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].hairM;
-        characterSelect.transform.Find("Character").Find("Hair D").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].hairD;
-        characterSelect.transform.Find("Character").Find("Hair Eyes").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].hairE;
+        PaletteColor customColor = new PaletteColor();
+        customColor.hairL = Color.black;
+        customColor.hairM = Color.black;
+        customColor.hairD = Color.black;
+        customColor.hairE = Color.black;
 
-        characterSelect.transform.Find("Character").Find("Body").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].bodyL;
-        characterSelect.transform.Find("Character").Find("Body (1)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].bodyM;
-        characterSelect.transform.Find("Character").Find("Body (2)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].bodyD;
+        paletteColors.Add(customColor);
 
-        characterSelect.transform.Find("Character").Find("Clothes").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].clothesL;
-        characterSelect.transform.Find("Character").Find("Clothes (1)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].clothesM;
-        characterSelect.transform.Find("Character").Find("Clothes (2)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].clothesD;
-        characterSelect.transform.Find("Character").Find("Clothes (3)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].clothesDD;
 
-        characterSelect.transform.Find("Character").Find("Misc").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].miscL;
-        characterSelect.transform.Find("Character").Find("Misc (1)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].miscD;
+        GenerateSprite(0);
 
         characterSelect.transform.Find("Colors").Find("Square (6)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[0].clothesL;
         characterSelect.transform.Find("Colors").Find("Square (5)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[1].clothesL;
@@ -140,7 +215,189 @@ public class PlayerController : MonoBehaviour
         characterSelect.transform.Find("Colors").Find("Square (2)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[4].clothesL;
         characterSelect.transform.Find("Colors").Find("Square (1)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[5].clothesL;
         characterSelect.transform.Find("Colors").Find("Square").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[6].clothesL;
-        characterSelect.transform.Find("Colors").Find("Square (3)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[7].clothesL;
+        characterSelect.transform.Find("Colors").Find("Square (3)").gameObject.GetComponent<SpriteRenderer>().color = Color.black;
+    }
+
+    void FixedUpdate()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            if (!escapeBuffer)
+            {
+                if (escape)
+                {
+                    SceneManager.LoadScene(0);
+                }
+            }
+        } else if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (changeCooldown <= 0)
+        {
+            if (selectingState != 1)
+            {
+                if (p1Move.x > 0)
+                {
+                    if (p1ColorSelectIndex == 3 || p1ColorSelectIndex == 7)
+                    {
+                        p1ColorSelectIndex -= 3;
+                    }
+                    else
+                    {
+                        p1ColorSelectIndex += 1;
+                    }
+
+                    changeCooldown = 0.15f;
+                    GenerateSprite(p1ColorSelectIndex);
+
+
+                }
+                else if (p1Move.x < 0)
+                {
+                    if (p1ColorSelectIndex == 0 || p1ColorSelectIndex == 4)
+                    {
+                        p1ColorSelectIndex += 3;
+                    }
+                    else
+                    {
+                        p1ColorSelectIndex -= 1;
+                    }
+
+                    changeCooldown = 0.15f;
+                    GenerateSprite(p1ColorSelectIndex);
+
+
+                }
+
+                if (p1Move.y > 0 || p1Move.y < 0)
+                {
+                    if (p1ColorSelectIndex >= 4)
+                    {
+                        p1ColorSelectIndex -= 4;
+                    }
+                    else
+                    {
+                        p1ColorSelectIndex += 4;
+                    }
+
+                    changeCooldown = 0.15f;
+                    GenerateSprite(p1ColorSelectIndex);
+
+
+                }
+            }
+        } else
+        {
+            changeCooldown -= Time.deltaTime;
+        }
+
+        if (p1Jump)
+        {
+            if (!jumpBuffer)
+            {
+                jumpBuffer = true;
+
+                switch (selectingState)
+                {
+                    case 0:
+                        characterSelect.transform.Find("Colors").gameObject.SetActive(false);
+                        characterSelect.transform.Find("Player Tag").gameObject.SetActive(true);
+
+                        float H, S, V;
+                        Color.RGBToHSV(paletteColors[p1ColorSelectIndex].clothesL, out H, out S, out V);
+                        if (V < 0.5f) V += 0.4f;
+
+                        characterSelect.transform.Find("Player Tag").gameObject.GetComponent<SpriteRenderer>().color = Color.HSVToRGB(H, S, V);
+
+                        selectingState = 1;
+                        playerManager.activePlayers--;
+                        break;
+                    case 1:
+                        if(playerManager.activePlayers == 0)
+                        {
+                            SceneManager.LoadScene(2);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } else
+        {
+            jumpBuffer = false;
+        }
+
+        if (p1Attack)
+        {
+            if (!attackBuffer)
+            {
+                attackBuffer = true;
+
+                switch (selectingState)
+                {
+                    case 1:
+                        characterSelect.transform.Find("Colors").gameObject.SetActive(true);
+                        characterSelect.transform.Find("Player Tag").gameObject.SetActive(false);
+
+                        selectingState = 0;
+                        playerManager.activePlayers++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        else
+        {
+            attackBuffer = false;
+        }
+
+        if (p1Fireball)
+        {
+            if (!splitKeyboard)
+            {
+                splitKeyboard = true;
+
+                playerManager.activePlayers++;
+                //add setting p2 char select
+            }
+        }
+    }
+
+    public void GenerateSprite(int index)
+    {
+        characterSelect.transform.Find("Character").Find("Hair L").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].hairL;
+        characterSelect.transform.Find("Character").Find("Hair M").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].hairM;
+        characterSelect.transform.Find("Character").Find("Hair D").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].hairD;
+        characterSelect.transform.Find("Character").Find("Hair Eyes").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].hairE;
+
+        characterSelect.transform.Find("Character").Find("Body").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].bodyL;
+        characterSelect.transform.Find("Character").Find("Body (1)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].bodyM;
+        characterSelect.transform.Find("Character").Find("Body (2)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].bodyD;
+
+        characterSelect.transform.Find("Character").Find("Clothes").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].clothesL;
+        characterSelect.transform.Find("Character").Find("Clothes (1)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].clothesM;
+        characterSelect.transform.Find("Character").Find("Clothes (2)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].clothesD;
+        characterSelect.transform.Find("Character").Find("Clothes (3)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].clothesDD;
+
+        characterSelect.transform.Find("Character").Find("Misc").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].miscL;
+        characterSelect.transform.Find("Character").Find("Misc (1)").gameObject.GetComponent<SpriteRenderer>().color = paletteColors[index].miscD;
+
+        int yLevel = 0;
+        if(p1ColorSelectIndex >= 4)
+        {
+            yLevel = 1;
+        }
+
+        int xLevel = p1ColorSelectIndex;
+        if (p1ColorSelectIndex >= 4)
+        {
+            xLevel -= 4;
+        }
+
+        characterSelect.transform.Find("Colors").Find("Selection").localPosition = new Vector3(-0.12f + 0.09f * xLevel, 0.33f - 0.09f * yLevel, transform.localPosition.y);
     }
     
     [System.Serializable]
--- boredjam/Assets/Scripts/PlayerManager.cs
+++ boredjam/Assets/Scripts/PlayerManager.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    
+    public int activePlayers = 0;
 }
